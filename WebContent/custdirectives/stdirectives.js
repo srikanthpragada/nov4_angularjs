10c0dedec80c801a036c346fb2fed0fa7b80e214
srikanthpragada <srikanthpragada@gmail.com>
2016-11-19 08:12:55 +0530

    19-NOV

---
+++ WebContent/custdirectives/stdirectives.js
@@ -0,0 +1,54 @@
+﻿var app = angular.module('stDirectives', [])
+.controller('Controller', ['$scope', function ($scope) {
+    $scope.today = new Date().toString();
+    $scope.name = "Srikanth Technologies"
+}]);
+
+app.directive('stAddress',
+ function () {
+	  return {
+			restrict : 'E', 
+		    template: '{{ name }} 905905700 ,  srikanth_technologies@yahoo.com'
+		  };
+  } // function
+); // end of directive
+
+
+
+app.directive('stToday', function () {
+    return {
+          restrict: 'E',
+          template: 'Today is : {{today}}'
+    };
+}
+);
+
+
+app.directive('stNow', function () {
+    return {
+        restrict: 'E',
+        link: function (scope, element, attrs) {
+            element.text(new Date());
+        }
+    };
+});
+
+
+app.directive('stTitle', function () {
+        return {
+            restrict: 'EA',
+            replace: true,
+            transclude :true,
+            template:  "<h1 class='title'><span ng-transclude></span></h1>"
+        };
+})
+
+app.directive('stCode', function () {
+    return {
+        restrict: 'EA',
+        transclude: true,
+        replace :true,
+        template : '<pre ng-transclude></pre>'
+    };
+});
+
---
+++ WebContent/route/app.js
@@ -0,0 +1,47 @@
+﻿var app = angular.module('routedemo', ['ngRoute']);
+
+app.config(
+    ['$routeProvider',
+     function ($routeProvider)
+     {
+      $routeProvider.
+        when('/first', {
+            templateUrl: 'first.html',
+            controller: 'FirstController'
+        }).
+        when('/second', {
+            templateUrl: 'second.html',
+            controller: 'SecondController'
+        }).
+        when('/list', {
+            templateUrl: 'list.html',
+            controller: 'ListController'
+        }).
+        otherwise({
+            redirectTo: '/first'
+        });
+     }
+    ]
+ );
+
+
+app.controller("ListController",
+		  function ($scope) {
+		      $scope.names = ["Java","Python","Ruby","C#","JavaScript"];
+		  }
+
+		);
+
+
+app.controller("FirstController",
+  function ($scope) {
+      $scope.message = "Message in First Controller!";
+  }
+
+);
+
+app.controller("SecondController",
+  function ($scope) {
+      $scope.message = "Message in Second Controller!";
+  }
+);
